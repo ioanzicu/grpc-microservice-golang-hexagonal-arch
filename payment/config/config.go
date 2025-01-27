c18ac9689a8b5933c9e1b9634e0ffe6db22ec497
Ioan Zicu <ioan.zicu94@gmail.com>
2025-01-27 07:56:07 +0100

    added payment config file content

---
+++ payment/config/config.go
@@ -0,0 +1,57 @@
+package config
+
+import (
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
+// init() will run only once.
+// init() will run after global variable initialization of each package and
+// before main() function. init() will only run if the package is imported.
+func init() {
+	fileName := ".env"
+	if fileExists(fileName) {
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatalf("Error loading .env file: %s", err)
+		}
+	}
+}
+
+func fileExists(fileName string) bool {
+	info, err := os.Stat(fileName)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
+}
+
+func GetEnv() string {
+	return getEnvironmentValue("ENV")
+}
+
+func GetDataSourceURL() string {
+	return getEnvironmentValue("DATA_SOURCE_URL")
+}
+
+func GetApplicationPort() int {
+	portStr := getEnvironmentValue("APPLICATION_PORT")
+	port, err := strconv.Atoi(portStr)
+
+	if err != nil {
+		log.Fatalf("port: %s is invalid", portStr)
+	}
+
+	return port
+}
+
+func getEnvironmentValue(key string) string {
+	if os.Getenv(key) == "" {
+		log.Fatalf("%s environment variable is missing.", key)
+	}
+
+	return os.Getenv(key)
+}
---
+++ payment/go.mod
@@ -0,0 +1,3 @@
+module github.com/ioanzicu/microservices/payment
+
+go 1.22.5
